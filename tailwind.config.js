d2946a49578470663539be63cf712f6171ec37e5
Nate Roberts <nathan.roberts.15@cnu.edu>
2024-09-28 16:53:30 -0400

    remove daisyui console messages

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,7 @@ module.exports = {
 		'./*.html',
 	],
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	daisyui: {
+		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+	},
 };
